a58717d01f445b5ff04605221628fef55870bd66
macnaga <macnaga6446@gmail.com>
2020-02-05 00:12:28 +0900

    initial commit.

---
+++ Assets/Resources/Bloom.shader
@@ -0,0 +1,150 @@
+﻿Shader "Bloom"
+{
+    Properties{
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        CGINCLUDE
+        #pragma vertex vert
+        #pragma fragment frag
+            
+        #include "UnityCG.cginc"
+
+        sampler2D _MainTex;
+        float4 _MainTex_ST;
+        float4 _MainTex_TexelSize;
+        sampler2D _SourceTex;
+        // _Thresholdを削除して_FilterParamsを追加
+        half4 _FilterParams;
+        float _Intensity;
+
+        struct appdata
+        {
+            float4 vertex : POSITION;
+            float2 uv : TEXCOORD0;
+        };
+
+        struct v2f
+        {
+            float2 uv : TEXCOORD0;
+            float4 vertex : SV_POSITION;
+        };
+
+        half3 sampleMain(float2 uv){
+            return tex2D(_MainTex, uv).rgb;
+        }
+
+        half3 sampleBox (float2 uv, float delta) {
+            float4 offset = _MainTex_TexelSize.xyxy * float2(-delta, delta).xxyy;
+            half3 sum = sampleMain(uv + offset.xy) + sampleMain(uv + offset.zy) + sampleMain(uv + offset.xw) + sampleMain(uv + offset.zw);
+            return sum * 0.25;
+        }
+
+        half getBrightness(half3 color){
+            return max(color.r, max(color.g, color.b));
+        }
+
+        v2f vert (appdata v)
+        {
+            v2f o;
+            o.vertex = UnityObjectToClipPos(v.vertex);
+            o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+            return o;
+        }
+
+        ENDCG
+
+
+        Cull Off
+        ZTest Always
+        ZWrite Off
+
+        Tags { "RenderType"="Opaque" }
+
+        // 0: 適用するピクセル抽出用のパス
+        Pass
+        {
+            CGPROGRAM
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // 色抽出にソフトニーを適用
+                half4 col = 1;
+                col.rgb = sampleBox(i.uv, 1.0);
+                half brightness = getBrightness(col.rgb);
+
+                half soft = brightness - _FilterParams.y;
+                soft = clamp(soft, 0, _FilterParams.z);
+                soft = soft * soft * _FilterParams.w;
+                half contribution = max(soft, brightness - _FilterParams.x);
+                contribution /= max(brightness, 0.00001);
+                return col * contribution;
+            }
+
+            ENDCG
+        }
+
+        // 1: ダウンサンプリング用のパス
+        Pass
+        {
+            CGPROGRAM
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                half4 col = 1;
+                col.rgb = sampleBox(i.uv, 1.0);
+                return col;
+            }
+
+            ENDCG
+        }
+
+        // 2: アップサンプリング用のパス
+        Pass
+        {
+            Blend One One
+
+            CGPROGRAM
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                half4 col = 1;
+                col.rgb = sampleBox(i.uv, 0.5);
+                return col;
+            }
+
+            ENDCG
+        }
+
+        // 3: 最後の一回のアップサンプリング用のパス
+        Pass
+        {
+            CGPROGRAM
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                half4 col = tex2D(_SourceTex, i.uv);
+                col.rgb += sampleBox(i.uv, 0.5) * _Intensity;
+                return col;
+            }
+
+            ENDCG
+        }
+
+        // 4: デバッグ用
+        Pass
+        {
+            CGPROGRAM
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                half4 col = 1;
+                col.rgb = sampleBox(i.uv, 0.5) * _Intensity;
+                return col;
+            }
+
+            ENDCG
+        }
+    }
+}
---
+++ Assets/Scripts/MyPostProcess.cs
@@ -0,0 +1,176 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+[ExecuteAlways]
+public class MyPostProcess : MonoBehaviour
+{
+    #region Bloom
+    [SerializeField] bool bloom;
+    [SerializeField, Range(1, 30)] int _iteration = 1;
+    [SerializeField, Range(0.0f, 1.0f)] float _threshold = 0.0f;
+    [SerializeField, Range(0.0f, 1.0f)] float _softThreshold = 0.0f;
+    [SerializeField, Range(0.0f, 10.0f)] float _intensity = 1.0f;
+    [SerializeField] bool _debug;
+    // 4点をサンプリングして色を作るマテリアル
+    [SerializeField] Material bloomMaterial;
+    #endregion
+    #region DOF
+    public bool dof;
+    public Material material;
+
+    private int _Direction;
+    private int _BlurTex;
+    #endregion
+
+    RenderTargetIdentifier[] ids = new RenderTargetIdentifier[30];
+    RenderTextureDescriptor[] descs = new RenderTextureDescriptor[30];
+
+    CommandBuffer cmd;
+    RenderTextureDescriptor sourceDesc = new RenderTextureDescriptor();
+    int sourceId = Shader.PropertyToID("Source");
+
+    Camera camera;
+    RenderTargetIdentifier srcId = new RenderTargetIdentifier(BuiltinRenderTextureType.CameraTarget);
+    RenderTargetIdentifier destId = new RenderTargetIdentifier(BuiltinRenderTextureType.CameraTarget);
+    int rt1Id = Shader.PropertyToID("_rt1");
+    int rt2Id = Shader.PropertyToID("_rt2");
+
+    void Awake()
+    {
+        camera = GetComponent<Camera>();
+        camera.depthTextureMode |= DepthTextureMode.Depth;
+        _Direction = Shader.PropertyToID("_Direction");
+        _BlurTex = Shader.PropertyToID("_BlurTex");
+    }
+
+    void OnEnable()
+    {
+        cmd = new CommandBuffer();
+        camera.AddCommandBuffer(CameraEvent.BeforeImageEffects, cmd);
+    }
+
+    void OnDisable()
+    {
+        camera.RemoveCommandBuffer(CameraEvent.BeforeImageEffects, cmd);
+    }
+
+    void OnPreRender()
+    {
+        BuildCommandBuffer();
+    }
+
+    void BuildCommandBuffer()
+    {
+        cmd.Clear();
+
+        if (bloom)
+        {
+            SetupBloom();
+        }
+
+        if (dof)
+        {
+            SetupDOF();
+        }
+    }
+
+    void SetupBloom()
+    {
+        sourceDesc.width = Screen.width;
+        sourceDesc.height = Screen.height;
+        sourceDesc.msaaSamples = 1;
+        sourceDesc.colorFormat = RenderTextureFormat.Default;
+        sourceDesc.useMipMap = false;
+        sourceDesc.dimension = TextureDimension.Tex2D;
+        sourceDesc.enableRandomWrite = false;
+        sourceDesc.depthBufferBits = 0;
+        sourceDesc.volumeDepth = 16;
+        sourceDesc.autoGenerateMips = false;
+        sourceDesc.msaaSamples = 1;
+        sourceDesc.sRGB = false;
+
+        cmd.GetTemporaryRT(sourceId, sourceDesc, FilterMode.Bilinear);
+        var currentSourceId = new RenderTargetIdentifier(BuiltinRenderTextureType.CameraTarget);
+
+        var filterParams = Vector4.zero;
+        var knee = _threshold * _softThreshold;
+        filterParams.x = _threshold;
+        filterParams.y = _threshold - knee;
+        filterParams.z = knee * 2.0f;
+        filterParams.w = 0.25f / (knee + 0.00001f);
+        bloomMaterial.SetVector("_FilterParams", filterParams);
+        bloomMaterial.SetFloat("_Intensity", _intensity);
+        cmd.SetGlobalTexture("_SourceTex", new RenderTargetIdentifier(BuiltinRenderTextureType.CameraTarget));//currentSourceId);
+
+        var width = sourceDesc.width;
+        var height = sourceDesc.height;
+
+        var pathIndex = 0;
+        var i = 0;
+        RenderTextureDescriptor currentDesc;
+        // ダウンサンプリング
+        for (; i < _iteration; i++)
+        {
+            width /= 2;
+            height /= 2;
+            if (width < 2 || height < 2)
+            {
+                break;
+            }
+            currentDesc = descs[i] = new RenderTextureDescriptor(width, height);
+
+            // 最初の一回は明度抽出用のパスを使ってダウンサンプリングする
+            pathIndex = i == 0 ? 0 : 1;
+            var descId = $"_{width}_{height}";
+            ids[i] = descId;
+
+            cmd.GetTemporaryRT(Shader.PropertyToID(descId), currentDesc, FilterMode.Bilinear);
+            cmd.Blit(currentSourceId, ids[i], bloomMaterial, pathIndex);
+            currentSourceId = descId;
+        }
+
+        // アップサンプリング
+        for (i -= 2; i >= 0; i--)
+        {
+            currentDesc = descs[i];
+
+            // Blit時にマテリアルとパスを指定する
+            cmd.Blit(currentSourceId, ids[i], bloomMaterial, 2);
+            cmd.ReleaseTemporaryRT(Shader.PropertyToID(currentSourceId.ToString()));
+            currentSourceId = ids[i];
+        }
+        // 最後にdestにBlit
+        pathIndex = _debug ? 4 : 3;
+        cmd.Blit(currentSourceId, destId, bloomMaterial, pathIndex);
+        cmd.ReleaseTemporaryRT(Shader.PropertyToID(currentSourceId.ToString()));
+    }
+
+    void SetupDOF()
+    {
+        var desc = new RenderTextureDescriptor(Screen.width / 2, Screen.height / 2);
+        cmd.GetTemporaryRT(rt1Id, desc, FilterMode.Bilinear);
+        cmd.GetTemporaryRT(rt2Id, desc, FilterMode.Bilinear);
+
+        var h = new Vector2(1, 0);
+        var v = new Vector2(0, 1);
+
+        // Scale Down
+        cmd.Blit(srcId, rt1Id);
+
+        // 0: Gaussian Blur
+        for (int i = 0; i < 3; i++)
+        {
+            material.SetVector(_Direction, h);
+            cmd.Blit(rt1Id, rt2Id, material, 0);
+            material.SetVector(_Direction, v);
+            cmd.Blit(rt2Id, rt1Id, material, 0);
+        }
+
+        // 1: DOF
+        cmd.SetGlobalTexture(_BlurTex, rt1Id);
+        cmd.Blit(srcId, destId, material, 1);
+
+        cmd.ReleaseTemporaryRT(rt2Id);
+        cmd.ReleaseTemporaryRT(rt1Id);
+    }
+}
